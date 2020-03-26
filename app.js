426c7da70a3911d947251040ad84acc018a27d69
ktapia576 <ktapia576@hotmail.com>
2020-03-26 16:29:48 -0400

    Change to port 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 // Load Express
 const app = express();
